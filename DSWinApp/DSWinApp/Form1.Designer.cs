b6b7ba2dcda90d4c285be98f7f8ca13adcc2046d
Web App Develop - .Net <tajdikahamed@gmail.com>
2019-05-23 10:53:13 +0600

    Class3_added

---
+++ DSWinApp/DSWinApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DSWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.ShowDupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(220, 94);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(445, 323);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(509, 34);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(671, 92);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 2;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(334, 34);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(157, 20);
+            this.inputTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(671, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reverse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(672, 177);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 5;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(671, 228);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 6;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // ShowDupButton
+            // 
+            this.ShowDupButton.Location = new System.Drawing.Point(671, 269);
+            this.ShowDupButton.Name = "ShowDupButton";
+            this.ShowDupButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowDupButton.TabIndex = 7;
+            this.ShowDupButton.Text = "Show Dup";
+            this.ShowDupButton.UseVisualStyleBackColor = true;
+            this.ShowDupButton.Click += new System.EventHandler(this.ShowDupButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowDupButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button ShowDupButton;
+    }
+}
+
---
+++ DSWinApp/DSWinApp/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSWinApp
+{
+    public partial class Form1 : Form
+    {
+        const int size = 100;
+        int[] number = new int[size];
+        int index2 = 0;
+
+        const int size3 = 100;
+        int[] number3 = new int[size3];
+        int index3 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            const int size = 10;
+            int[] number = new int[size];
+
+            number[0] = 3;
+            number[2] = 4;
+            number[4] = 9;
+            string message = "";
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] !=0)
+                {
+                    message = message + "Value of index " + index.ToString() + " :" + number[index].ToString() + "\n";
+                }
+            }
+
+
+
+            richTextBox1.Text = message;
+            */
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            string message = "";
+            message += "Disply : \n";
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+                {
+                    message = message + "Value of index " + index.ToString() + " :" + number[index].ToString() + "\n";
+                }
+            }
+
+
+
+
+            richTextBox1.AppendText(message);
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            number[index2] = Convert.ToInt32(inputTextBox.Text);
+            index2++;
+            inputTextBox.Text = "";
+
+            string message = "";
+            //message += "Input Value : \n";
+
+       
+                    message = message + "Input Value in index " + index2.ToString() + " :" + number[index2-1].ToString() + "\n";
+
+            //richTextBox1.Text = message;
+            richTextBox1.AppendText(message);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string message = "";
+            message += "Disply in Reverse : \n";
+            for (int index = number.Length - 1; index >= 0; index--)
+            {
+                if (number[index] != 0)
+                {
+                    message = message + "Value of index " + index.ToString() + " :" + number[index].ToString() + "\n";
+                }
+            }
+
+            richTextBox1.AppendText(message);
+
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+                {
+                    sum += number[index];
+                }
+            }
+
+            richTextBox1.AppendText("\nSum of the array value :"+ sum.ToString()+"\n");
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+                {
+                    number3[index]  = number[index];
+                }
+            }
+            richTextBox1.AppendText("Done Copy One array to Another\n");
+        }
+
+        private void ShowDupButton_Click(object sender, EventArgs e)
+        {
+
+            string message = "";
+            message += "Disply Copy One : \n";
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number3[index] != 0)
+                {
+                    message = message + "Value of index " + index.ToString() + " :" + number3[index].ToString() + "\n";
+                }
+            }
+            richTextBox1.AppendText(message);
+        }
+    }
+}
