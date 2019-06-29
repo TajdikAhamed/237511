20e375d842074a3a958b838c7afa71ac6d9ec6b7
unknown <tajdikahamed@gmail.com>
2019-06-30 01:58:23 +0600

    Ass adde oop

---
+++ Data Hiding Constructor and Property/RefrigeratorAppPractice3/RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigerator
+    {
+        private double MaxWeight { get; set; }
+        public double CureentWeight { get; private set; }
+
+        public Refrigerator(double maxWeight) {
+            this.MaxWeight = maxWeight;
+        }
+
+        public bool PutItems(int quentity,double unitWeight)
+        {
+            if (this.CureentWeight + (quentity * unitWeight) > this.MaxWeight)
+            {
+                return false;
+            }
+            this.CureentWeight = this.CureentWeight + (quentity * unitWeight);
+            return true;
+         }
+
+
+        public double GetRemain()
+        {
+            return this.MaxWeight - this.CureentWeight;
+        }
+    }
+}
--- Data Hiding Constructor and Property/RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
+++ Data Hiding Constructor and Property/RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -12,9 +12,46 @@ namespace RefrigeratorAppPractice3
 {
     public partial class RefrigeratorUi : Form
     {
+        Refrigerator Refrigerator;
         public RefrigeratorUi()
         {
             InitializeComponent();
         }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            double maxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+            Refrigerator = new Refrigerator(maxWeight);
+            maxWeightTakeTextBox.Text = "";
+            Show();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            int itemQ = Convert.ToInt32(itemTextBox.Text);
+            double Weight = Convert.ToDouble(weightTextBox.Text);
+
+            bool flag = Refrigerator.PutItems(itemQ, Weight);
+            if (flag)
+            {
+                Show();
+            }
+            else
+            {
+                MessageBox.Show("Your given amount can’t be entered because it will be over flown.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+            
+        }
+        private void Show()
+        {
+            currentWeightTextBox.Text = Refrigerator.CureentWeight.ToString();
+            double rem = Refrigerator.GetRemain();
+            remainingWeightTextBox.Text = rem.ToString();
+
+        }
+
+        
     }
 }
